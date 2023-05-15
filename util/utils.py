8037898c8a695e768e27ff7be967ea34768cad5b
Carlos Garcia <carlosgm128@gmail.com>
2023-05-14 22:11:33 -0400

    added multiple tries to insert the file path

--- util/utils.py
+++ util/utils.py
@@ -44,18 +44,28 @@ class Utils:
             method created to retreive data from an archive or a path indicated
         """
         file = None
-        if len(sys.argv) > 1 and sys.argv[1].find('--path') != -1:
-            path = sys.argv[1].rsplit('=')[1]
-            Utils.check_extension(path)
-        else:
-            path = input("inserte la ruta del archivo de pagos para procesar ")
-            Utils.check_extension(path)
-        file = open(path, 'rt', encoding='UTF-8')
+        tries = 3
+        while tries != 0:
+            try:
+                if len(sys.argv) > 1 and sys.argv[1].find('--path') != -1:
+                    path = sys.argv[1].rsplit('=')[1]
+                    Utils.check_extension(path)
+                else:
+                    path = input("inserte la ruta del archivo de pagos para procesar ")
+                    Utils.check_extension(path)
+                file = open(path, 'rt', encoding='UTF-8')
+            except FileNotFoundError:
+                print(f'Archivo no encontrado. {tries -1} intentos restantes')
+                if tries == 1:
+                    print('numero de intentos posibles terminados\nsaliendo...')
+                    sys.exit()
+            finally:
+                tries -= 1
         try:
             data = file.readlines()
             return data
-        except FileNotFoundError:
-            raise FileNotFoundError(f"Archivo con el nombre de {path} no encontrado")
+        except FileNotFoundError as ex:
+            raise FileNotFoundError(f"Archivo con el nombre de {path} no encontrado\nError\n {ex}")
         finally:
             file.close()
 
