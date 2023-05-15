d52757a8f741d0bff2d996af1fba939312287e49
Carlos Garcia <carlosgm128@gmail.com>
2023-05-14 20:14:03 -0400

    re-factor due to utils structure change

--- main.py
+++ main.py
@@ -3,18 +3,18 @@
     pagos por hora de jornada y dia de la jornada
 """
 import logging
-from util.utils import get_data_from_archive, clean_data, get_name, get_days_worked, generate_days_worked
+from util.utils import Utils
 from models.User import User
 from models.day import Day
 from payment.Payment import Payment
 
 
 def main():
-    archive = get_data_from_archive()
-    data = clean_data(archive)
+    archive = Utils.get_data_from_archive()
+    data = Utils.clean_data(archive)
     for user in data:
-        user_name = get_name(user)
-        days: list[Day] = generate_days_worked(get_days_worked(user))
+        user_name = Utils.get_name(user)
+        days: list[Day] = Utils.generate_days_worked(Utils.get_days_worked(user))
         user_instance = User(user_name, days)
         payment = Payment.process_payment(user_instance)
         print(f"EL monto a para a {user_instance.get_name()} es : {'{:.2f}'.format(payment)} USD")
--- payment/Payment.py
+++ payment/Payment.py
@@ -1,5 +1,6 @@
+from const.shift import Constantes
 from models.User import User
-from util.utils import *
+from util.utils import Utils
 
 
 class Payment:
@@ -12,13 +13,13 @@ class Payment:
         total_to_pay = 0
         total_hours_work = 0
         for day in user.get_worked():
-            day_hours = get_worked_time(day, Constantes.day_shift())
-            night_hours = get_worked_time(day, Constantes.night_shift())
-            midnight_hours = get_worked_time(day, Constantes.midnight_shift())
+            day_hours = Utils.get_worked_time(day, Constantes.day_shift())
+            night_hours = Utils.get_worked_time(day, Constantes.night_shift())
+            midnight_hours = Utils.get_worked_time(day, Constantes.midnight_shift())
 
-            payment_block_day = get_price_per_shift(day, Constantes.day)
-            payment_block_night = get_price_per_shift(day, Constantes.night)
-            payment_block_midnight = get_price_per_shift(day, Constantes.midnight)
+            payment_block_day = Utils.get_price_per_shift(day, Constantes.day)
+            payment_block_night = Utils.get_price_per_shift(day, Constantes.night)
+            payment_block_midnight = Utils.get_price_per_shift(day, Constantes.midnight)
 
             produced_day = day_hours * payment_block_day
             produced_night = night_hours * payment_block_night
