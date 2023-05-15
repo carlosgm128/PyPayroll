86e812f3044e120b8c033ff0ee903360fd750ccf
Carlos Garcia <carlosgm128@gmail.com>
2023-05-15 11:53:41 -0400

    typo fix

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
     calculadora para determinar el salario de un empleado basado en bloques de jornadas con diferentes y
     pagos por hora de jornada y dia de la jornada
 """
-import logging
 from util.utils import Utils
 from models.User import User
 from models.day import Day
@@ -17,7 +16,7 @@ def main():
         days: list[Day] = Utils.generate_days_worked(Utils.get_days_worked(user))
         user_instance = User(user_name, days)
         payment = Payment.process_payment(user_instance)
-        print(f"EL monto a para a {user_instance.get_name()} es : {'{:.2f}'.format(payment)} USD")
+        print(f"EL monto a pagar a {user_instance.get_name()} es : {'{:.2f}'.format(payment)} USD")
 
 
 if __name__ == '__main__':
